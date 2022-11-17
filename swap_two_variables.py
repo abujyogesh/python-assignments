ee62dd01c982668eb4c9abf5ceba6f6216967979
abujyogesh <118466149+abujyogesh@users.noreply.github.com>
2022-11-17 22:21:50 +0530

    Add files via upload

---
+++ swap_two_variables.py
@@ -0,0 +1,13 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("numbers before swap::")
+print("first number::",a)
+print("second number::",b)
+
+t=a
+a=b
+b=t
+
+print("no after swap")
+print("first number::",a)
+print("second number::",b)
