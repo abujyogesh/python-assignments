4c4f7d9f04dafdbbf8d7e468ae4dd66287392a40
abujyogesh <118466149+abujyogesh@users.noreply.github.com>
2022-11-19 21:38:35 +0530

    Add files via upload

---
+++ 18.function_for_return_new_list_with_unique_elements_from_first_list.py
@@ -0,0 +1,10 @@
+def unique_list(numbers):
+    unique=[]
+    for item in numbers:
+        if item not in unique:
+            unique.append(item)
+    return unique
+
+
+
+print("unique elements::",unique_list([10,20,30,40,50,50,40,30,20,10]))
