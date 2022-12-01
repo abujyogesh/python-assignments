a593ea4d066fbe2d027bccf96d9c306c693f9b49
abujyogesh <118466149+abujyogesh@users.noreply.github.com>
2022-12-01 21:25:59 +0530

    Delete 17.count_numbers_of_matching_elements_in_pair_of_string.py

--- 17.count_numbers_of_matching_elements_in_pair_of_string.py
+++
@@ -1,9 +0,0 @@
-a=input("enter first string:")
-b=input("enter second string:")
-c=list([])
-for ch in a:
-    if ch in b:
-        c.append(ch)
-
-print("number of matching elements:",len(c))
-        
